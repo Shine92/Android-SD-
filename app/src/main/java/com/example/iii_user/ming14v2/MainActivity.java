a27254f96871b23395e9dc960cd4f6f8ab3c9bd2
Shine92 <msn8971@gmail.com>
2016-06-01 16:20:52 +0800

    Android14
    存取檔案至SD卡
    ProgressDialog 使用
    Environment.getExternalStorageDirectory();取得外部SD卡
    android 20 影片範例

---
+++ app/src/main/java/com/example/iii_user/ming14v2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.iii_user.ming14v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
